65519e48313f8a48d3bec401c786bce63b08e200
Anil Kumar <72590342+diablo3775@users.noreply.github.com>
2023-02-15 21:35:57 +0530

    Add files via upload

---
+++ day5/src/components/Body.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import RestaurantCard from './RestaurantCard';
+import restaurantList from './data'
+
+const Body = () => {
+  const [searchText, setSearchText] = useState("")
+  const [restaurants, setRestaurants] = useState(restaurantList)
+  
+  function filterData(searchText, restaurants) {
+    const filterData = restaurants.filter((restaurant) => restaurant.data.name.toLowerCase().includes(searchText));
+    console.log(filterData)
+    return filterData;
+  }
+  
+  filterData("Udipi's Upahar", restaurants)
+
+    return (
+      <>
+      <div className='search-container'>
+        <input className='search-input' value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+        <button className='search-btn' onClick={() => setRestaurants(filterData(searchText, restaurants))}>Search</button>  
+      </div>
+      <div className="restaurantList">
+        {restaurants.map((restaurant) => (
+          <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+        ))}
+      </div>
+      </>
+    );
+  };
+  
+
+export default Body
